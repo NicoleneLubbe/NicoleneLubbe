e1f21081462960c92c28a70aa2a54212b57ac0d7
NicoleneLubbe <93667800+NicoleneLubbe@users.noreply.github.com>
2022-10-24 17:56:17 +0200

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,12 @@
+<?php
+include "connect.php";
+$id = $_GET['id'];
+$sql = "DELETE FROM 'crud' WHERE id = $id";
+$result = mysqli_query($conn, $sql);
+if($result){
+    header("Location: index.php?msg=Record Deleted Successfully");
+}
+else{
+    echo "Failed: " . mysqli_error($conn);
+}
+?>
